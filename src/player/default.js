b4649b0d05b4ca0806742219be883d6c03e3c7f4
wayerr <radiofun@ya.ru>
2017-03-12 23:56:33 +0300

    update default player too

--- src/player/default.js
+++ src/player/default.js
@@ -17,46 +17,21 @@
 
 class DefaultPlayer {
 
-    static factory() {
-        let player = new DefaultPlayer();
-        console.debug("Begin create proxy");
-        let w = content.unwrap(window);
-        let handler = {
-            construct: (target, argumentsList, newTarget) => {
-                try {
-                    let res = Reflect.construct(target, argumentsList);
-                    console.debug("New audio created: ", res);
-                    res.addEventListener("play", (e) => {
-                        console.debug("Change player due to 'play' event:", e);
-                        player.setMedia(e.target);
-                    });
-                    player.setMedia(res);
-                    return res;
-                } catch (e) {
-                    console.debug("Cannot create", e);
-                }
-            }
-        };
-        w.Audio = new w.Proxy(w.Audio, cloneInto(handler, w, {cloneFunctions:true}));
-        console.debug("End create proxy");
-        return player;
-    }
-
     constructor() {
         this.callback = (e) => {
             content.playerUpdated();
         };
         // api has event like PROGRESS but it too noizy, so we use simply timeout per 2 seconds
 
-        let iid = window.setInterval(() => {
+        this.iid = window.setInterval(() => {
             let m = this.getMedia();
             if(m && !m.played) {
                 // we ignore event when nothing is played
                 return;
             }
-            content.playerUpdated();
+            foxhorn.playerUpdated();
         }, 2000);
-        content.onUnload(() => window.clearInterval(iid));
+        
     }
 
     findMedia() {
@@ -108,21 +83,52 @@ class DefaultPlayer {
             tracks: [track]
         };
     }
-    
-    setMedia(newmedia) {
-        if(this._media === newmedia) {
-            return;
-        }
+
+    unsubscribe() {
         let old = this._media;
         if(old) {
             old.removeEventListener("changed", this.callback);
             old.removeEventListener("load", this.callback);
         }
+    }
+
+    close() {
+        window.clearInterval(this.iid);
+        this.unsubscribe();
+    }
+    
+    setMedia(newmedia) {
+        if(this._media === newmedia) {
+            return;
+        }
+        this.unsubscribe();
         this._media = newmedia;
         newmedia.addEventListener("changed", this.callback);
         newmedia.addEventListener("load", this.callback);
-        content.playerUpdated();
+        foxhorn.playerUpdated();
     }
 }
 
-content.initPlayer(DefaultPlayer);
+(function() {
+    let player = new DefaultPlayer();
+    console.debug("Begin create proxy");
+    let handler = {
+        construct: (target, argumentsList, newTarget) => {
+            try {
+                let res = Reflect.construct(target, argumentsList);
+                console.debug("New audio created: ", res);
+                res.addEventListener("play", (e) => {
+                    console.debug("Change player due to 'play' event:", e);
+                    player.setMedia(e.target);
+                });
+                player.setMedia(res);
+                return res;
+            } catch (e) {
+                console.debug("Cannot create", e);
+            }
+        }
+    };
+    Audio = new Proxy(Audio, handler);
+    console.debug("End create proxy");
+    foxhorn.setPlayer(player);
+})();
